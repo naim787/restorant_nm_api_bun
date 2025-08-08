3ad333e6845d342f458431a8d38ab44e0bab68bd
naim <naimmmmab@gmail.com>
2025-08-09 07:49:14 +0800

    Update: 8/9/2025, 7:47:09 AM

--- src/handler/product/test.js
+++ src/handler/product/test.js
@@ -1,2 +1,11 @@
 import { unlinkSync, existsSync } from 'fs';
 import { join } from 'path';
+
+const imagePath = join('public', ""); // misalnya 'public/uploads/123.jpg'
+
+if (existsSync(imagePath)) {
+    unlinkSync(imagePath);
+    console.log("🧹 Gambar dihapus:", imagePath);
+} else {
+    console.warn("⚠️ File gambar tidak ditemukan:", imagePath);
+}
