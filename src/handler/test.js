00a2f5e1ecb4460fcc0cef7a7533a5639517b21a
naim <naimmmmab@gmail.com>
2025-08-09 13:38:54 +0800

    Update: 8/9/2025, 7:47:09 AM

--- src/handler/test.js
+++ src/handler/test.js
@@ -1,1 +1,6 @@
-const
+fetch('http://localhost:3000/users/123', {
+        method: 'DELETE'
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err));
