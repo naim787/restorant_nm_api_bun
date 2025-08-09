d3d72d00aed86fb345484a3b568faa7313a979b8
naim <naimmmmab@gmail.com>
2025-08-09 09:43:10 +0800

    Update: 8/9/2025, 7:47:09 AM

--- src/handler/uRoutes.js
+++ src/handler/uRoutes.js
@@ -1,5 +1,6 @@
 import { Elysia } from 'elysia';
 import { getUsers, createUser } from './user/uHandler.js';
+import { getUsers, createUser } from './user/uHandler.js';
 
 export const userRoutes = new Elysia()
     .get('/users', getUsers)
