f1745e0e9ab36de02828b5718493307bc8624960
naim <naimmmmab@gmail.com>
2025-08-09 13:40:29 +0800

    Update: 8/9/2025, 7:47:09 AM

--- src/handler/test.js
+++ src/handler/test.js
@@ -1,6 +1,19 @@
-fetch('http://localhost:3000/users/123', {
-        method: 'DELETE'
-    })
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err));
+import { test, expect } from "bun:test";
+
+test("hapus user yang ada di DB", async() => {
+    const userId = "123"; // ID user yang mau dihapus
+
+    const res = await fetch(`http://localhost:3000/users/${userId}`, {
+        method: "DELETE"
+    });
+
+    // Pastikan status HTTP benar
+    expect(res.status).toBe(200);
+
+    const data = await res.json();
+
+    // Pastikan responnya sesuai
+    expect(data).toHaveProperty("message", "User berhasil dihapus");
+    expect(data).toHaveProperty("data");
+    expect(data.data).toHaveProperty("id", userId);
+});
