74f6d82bb875eec0bc428274b991f6d708b8513e
naim <naimmmmab@gmail.com>
2025-08-09 15:28:31 +0800

    Update: 8/9/2025, 7:47:09 AM

--- src/handler/user/uGet.js
+++ src/handler/user/uGet.js
@@ -1,1 +1,19 @@
-............
+import { prisma } from '../../conf/database.js';
+
+export const getUsers = {
+    handler: async({set }) => {
+        try {
+            const users = await prisma.users.findMany();
+
+            return {
+                message: "Users retrieved successfully",
+                data: users
+            };
+        } catch (error) {
+            set.status = 500;
+            return {
+                error: "Failed to retrieve users"
+            };
+        }
+    }
+};
