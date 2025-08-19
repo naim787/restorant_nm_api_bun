4a0c2daf6cfb83b84655bda1774c6303807247e2
naim <naimmmmab@gmail.com>
2025-08-20 04:07:24 +0800

    Update: 8/19/2025, 10:53:14 AM

--- src/handler/websoket/wHandler.js
+++ src/handler/websoket/wHandler.js
@@ -41,9 +41,9 @@ export const websocketHandler = {
 
             if (orders.status !== "pendigg") {
 
-            }
+            } else
             // Simpan ke database
-            const savedOrder = await prisma.order.create({
+                const savedOrder = await prisma.order.create({
                 data: {
                     table_id: orders.table_id,
                     waiter_name: orders.waiter_name,
