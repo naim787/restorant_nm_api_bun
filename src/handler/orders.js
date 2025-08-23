f7d3115972275a4c65cc95190487263e0ed3ec6f
naim <naimmmmab@gmail.com>
2025-08-23 11:05:34 +0800

    Update: 8/23/2025, 10:12:51 AM

--- src/handler/orders.js
+++ src/handler/orders.js
@@ -2,4 +2,4 @@ import Elysia from 'elysia';
 import { orders } from './orders/oGet.js';
 
 export const ordersRoutes = new Elysia()
-    .get('/orders', orders.handler)
+    .get('/orders', orders.handler);
