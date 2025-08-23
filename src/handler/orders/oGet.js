f2a7f399358022a761f62a97561ab0864f3f460a
naim <naimmmmab@gmail.com>
2025-08-23 11:04:24 +0800

    Update: 8/23/2025, 10:12:51 AM

--- src/handler/orders/oGet.js
+++ src/handler/orders/oGet.js
@@ -3,7 +3,7 @@ import { prisma } from '../../conf/database.js';
 export const orders = {
     handle: async({set }) => {
         try {
-            const order = await prisma.order.findMany();
+            const order = await prisma.rder.findMany();
 
             return {
                 message: "berhasil meggambil data order",
